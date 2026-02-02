32d59c871a113f28fe4e757bf07be11213995490
mpark-oss <mpark@hist.co.kr>
2026-02-02 21:25:23 +0900

    Update MakerView.vue

--- src/views/MakerView.vue
+++ src/views/MakerView.vue
@@ -115,7 +115,7 @@ import { db } from '../firebase';
 import { collection, addDoc, onSnapshot, query, orderBy, deleteDoc, doc } from "firebase/firestore";
 
 const API_KEY = import.meta.env.VITE_LOSTARK_API_KEY;; 
-const PROXY_URL = "https://cors-anywhere.herokuapp.com/";
+const PROXY_URL = "const PROXY_URL = "https://my-cors.vercel.app/";
 
 const searchName = ref('');
 const charList = ref([]);

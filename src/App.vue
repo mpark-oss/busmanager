4fb521883d4347f40b7c8e1e9df9982a9b028118
ZZISHE <audwls7505@naver.com>
2026-03-09 08:51:26 +0900

    20260309 UPDATE

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,97 @@
 <template>
   <v-app :theme="theme">
-    <v-overlay :model-value="!isLoggedIn" persistent class="align-center justify-center" scrim="#000" z-index="9999">
+    <v-overlay
+      :model-value="!isLoggedIn"
+      persistent
+      class="align-center justify-center"
+      scrim="#000"
+      z-index="9999"
+    >
       <v-card class="pa-10 text-center rounded-xl" elevation="24" width="450">
-
-        <v-card width="120" height="120" class="mx-auto mb-6 rounded-lg elevation-4 border border-opacity-25"
-          color="grey-darken-4">
-          <v-img src="/stamp_logo.png" alt="흐흣 로고" cover></v-img>
+        <v-card
+          width="120"
+          height="120"
+          class="mx-auto mb-6 rounded-lg d-flex align-center justify-center elevation-0"
+          style="background-color: transparent !important"
+        >
+          <v-img
+            src="/stamp_logo.png"
+            width="100%"
+            height="100%"
+            class="stamp-no-bg"
+          ></v-img>
         </v-card>
 
         <h2 class="text-h5 font-weight-black mb-2">흐흣 수용소 입장</h2>
         <p class="text-body-2 text-medium-emphasis mb-8">
-          흐흣 길드원만 이용 가능한 시스템입니다.<br>디스코드 인증을 완료해주세요.
+          흐흣 길드원만 이용 가능한 시스템입니다.<br />디스코드 인증을
+          완료해주세요.
         </p>
 
-        <v-btn block color="indigo" size="x-large" variant="flat" class="font-weight-black rounded-lg"
-          @click="handleLogin" prepend-icon="mdi-discord">
-          디스코드로 길드원 인증하기
+        <v-btn
+          block
+          color="#5865F2"
+          size="x-large"
+          variant="flat"
+          class="font-weight-black rounded-lg"
+          @click="handleLogin"
+        >
+          <template #prepend>
+            <svg
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              fill="white"
+              class="mr-2"
+            >
+              <path
+                d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028 14.09 14.09 0 0 0 1.226-1.994.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.196.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03zM8.02 15.33c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.956 2.419-2.157 2.419zm7.975 0c-1.183 0-2.157-1.085-2.157-2.419 0-1.333.955-2.419 2.157-2.419 1.21 0 2.176 1.096 2.157 2.42 0 1.333-.946 2.419-2.157 2.419z"
+              />
+            </svg>
+          </template>
+
+          Discord Login
         </v-btn>
       </v-card>
     </v-overlay>
 
     <template v-if="isLoggedIn">
-      <v-app-bar :color="theme === 'light' ? 'primary' : '#111111'" elevation="2" border>
-        <v-app-bar-title class="font-weight-black" style="flex: none !important; margin-right: 24px">
+      <v-app-bar
+        :color="theme === 'light' ? 'primary' : '#111111'"
+        elevation="2"
+        border
+      >
+        <v-app-bar-title
+          class="font-weight-black"
+          style="flex: none !important; margin-right: 24px"
+        >
           <div class="d-flex flex-row align-center">
-            <v-img src="/stamp_logo.png" alt="흐흣 로고" width="50" height="50"
-              class="me-3 rounded-lg border border-opacity-25" cover></v-img>
+            <v-img
+              src="/stamp_logo.png"
+              alt="흐흣 로고"
+              width="50"
+              height="50"
+              class="me-3 rounded-lg border border-opacity-25"
+              cover
+            ></v-img>
             <span class="text-h6">흐흣 수용소</span>
           </div>
         </v-app-bar-title>
 
         <div class="d-flex align-center">
-          <v-menu v-if="mainCharSlots.length > 0" offset-y transition="scale-transition">
+          <v-menu
+            v-if="mainCharSlots.length > 0"
+            offset-y
+            transition="scale-transition"
+          >
             <template v-slot:activator="{ props }">
-              <v-btn v-bind="props" variant="flat" color="blue-darken-2" class="rounded-pill px-4" size="small">
+              <v-btn
+                v-bind="props"
+                variant="flat"
+                color="blue-darken-2"
+                class="rounded-pill px-4"
+                size="small"
+              >
                 <v-avatar size="24" class="me-2" v-if="getCurrentSlot">
                   <v-img :src="getCurrentSlot.img" cover></v-img>
                 </v-avatar>
@@ -45,26 +103,47 @@
             </template>
 
             <v-list class="pa-2 rounded-xl mt-2" min-width="200" elevation="10">
-              <v-list-item v-for="char in mainCharSlots" :key="char.name" @click="switchAccount(char.name)" :class="currentMainName === char.name
-                ? 'bg-orange-lighten-5 text-orange-darken-4'
-                : ''
-                " class="rounded-lg mb-1">
+              <v-list-item
+                v-for="char in mainCharSlots"
+                :key="char.name"
+                @click="switchAccount(char.name)"
+                :class="
+                  currentMainName === char.name
+                    ? 'bg-orange-lighten-5 text-orange-darken-4'
+                    : ''
+                "
+                class="rounded-lg mb-1"
+              >
                 <template v-slot:prepend>
-                  <v-avatar size="32" border><v-img :src="char.img" cover></v-img></v-avatar>
+                  <v-avatar size="32" border
+                    ><v-img :src="char.img" cover></v-img
+                  ></v-avatar>
                 </template>
                 <v-list-item-title class="font-weight-black text-body-2">{{
                   char.name
                 }}</v-list-item-title>
-                <v-list-item-subtitle style="font-size: 0.65rem">Lv. {{ char.level }}</v-list-item-subtitle>
+                <v-list-item-subtitle style="font-size: 0.65rem"
+                  >Lv. {{ char.level }}</v-list-item-subtitle
+                >
               </v-list-item>
               <v-divider class="my-2"></v-divider>
-              <v-list-item prepend-icon="mdi-cog-outline" title="계정 추가/삭제" @click="charSettingDialog = true"
-                class="rounded-lg"></v-list-item>
+              <v-list-item
+                prepend-icon="mdi-cog-outline"
+                title="계정 추가/삭제"
+                @click="charSettingDialog = true"
+                class="rounded-lg"
+              ></v-list-item>
             </v-list>
           </v-menu>
 
-          <v-btn v-else variant="tonal" color="orange-darken-2" prepend-icon="mdi-account-plus"
-            class="rounded-pill font-weight-black" @click="charSettingDialog = true">
+          <v-btn
+            v-else
+            variant="tonal"
+            color="orange-darken-2"
+            prepend-icon="mdi-account-plus"
+            class="rounded-pill font-weight-black"
+            @click="charSettingDialog = true"
+          >
             대표 캐릭터를 설정하세요
           </v-btn>
         </div>
@@ -72,13 +151,26 @@
         <v-spacer></v-spacer>
 
         <v-btn to="/" prepend-icon="mdi-sword-cross">공격대 만들기</v-btn>
-        <v-btn to="/schedule" prepend-icon="mdi-calendar-clock">버스 스케줄</v-btn>
-        <v-btn prepend-icon="mdi-clipboard-check-multiple" to="/homework">숙제 스케줄</v-btn>
-        <v-btn to="/homework-schedule" prepend-icon="mdi-format-list-checks">개인 숙제 관리</v-btn>
+        <v-btn to="/schedule" prepend-icon="mdi-calendar-clock"
+          >버스 스케줄</v-btn
+        >
+        <v-btn prepend-icon="mdi-clipboard-check-multiple" to="/homework"
+          >숙제 스케줄</v-btn
+        >
+        <v-btn to="/homework-schedule" prepend-icon="mdi-format-list-checks"
+          >개인 숙제 관리</v-btn
+        >
         <v-btn to="/board" prepend-icon="mdi-calculator">쌀산기</v-btn>
         <v-btn to="/guestbook" prepend-icon="mdi-message-draw">커뮤니티</v-btn>
 
-        <v-btn icon @click="handleLogout" class="ms-2" color="" variant="text" title="로그아웃">
+        <v-btn
+          icon
+          @click="handleLogout"
+          class="ms-2"
+          color=""
+          variant="text"
+          title="로그아웃"
+        >
           <v-icon>mdi-logout</v-icon>
         </v-btn>
 
@@ -93,8 +185,15 @@
         <router-view></router-view>
 
         <v-fade-transition>
-          <v-btn v-show="showFab" icon="mdi-chevron-up" color="primary" size="large" elevation="8" class="scroll-to-top"
-            @click="scrollToTop"></v-btn>
+          <v-btn
+            v-show="showFab"
+            icon="mdi-chevron-up"
+            color="primary"
+            size="large"
+            elevation="8"
+            class="scroll-to-top"
+            @click="scrollToTop"
+          ></v-btn>
         </v-fade-transition>
       </v-main>
     </template>
@@ -107,27 +206,59 @@
         </v-card-title>
         <v-card-text>
           <div class="d-flex align-center gap-2 mb-4">
-            <v-text-field v-model="searchCharName" label="새 대표 캐릭터명" variant="outlined" hide-details density="compact"
-              color="orange-darken-2" @keyup.enter="addNewSlot"></v-text-field>
-            <v-btn color="orange-darken-2" icon="mdi-account-search" variant="flat" class="ms-2" :loading="isFetching"
-              @click="addNewSlot"></v-btn>
+            <v-text-field
+              v-model="searchCharName"
+              label="새 대표 캐릭터명"
+              variant="outlined"
+              hide-details
+              density="compact"
+              color="orange-darken-2"
+              @keyup.enter="addNewSlot"
+            ></v-text-field>
+            <v-btn
+              color="orange-darken-2"
+              icon="mdi-account-search"
+              variant="flat"
+              class="ms-2"
+              :loading="isFetching"
+              @click="addNewSlot"
+            ></v-btn>
           </div>
           <v-divider v-if="mainCharSlots.length > 0" class="my-4"></v-divider>
           <v-list v-if="mainCharSlots.length > 0" class="bg-transparent pa-0">
-            <v-list-item v-for="char in mainCharSlots" :key="char.name" class="bg-grey-darken-4 rounded-lg mb-2 pa-2">
-              <template v-slot:prepend><v-avatar size="40" border><v-img :src="char.img"
-                    cover></v-img></v-avatar></template>
+            <v-list-item
+              v-for="char in mainCharSlots"
+              :key="char.name"
+              class="bg-grey-darken-4 rounded-lg mb-2 pa-2"
+            >
+              <template v-slot:prepend
+                ><v-avatar size="40" border
+                  ><v-img :src="char.img" cover></v-img></v-avatar
+              ></template>
               <v-list-item-title class="font-weight-bold text-subtitle-2">{{
                 char.name
               }}</v-list-item-title>
-              <v-list-item-subtitle class="text-caption">Lv. {{ char.level }}</v-list-item-subtitle>
-              <template v-slot:append><v-btn icon="mdi-delete-outline" variant="text" color="error" size="small"
-                  @click="removeSlot(char.name)"></v-btn></template>
+              <v-list-item-subtitle class="text-caption"
+                >Lv. {{ char.level }}</v-list-item-subtitle
+              >
+              <template v-slot:append
+                ><v-btn
+                  icon="mdi-delete-outline"
+                  variant="text"
+                  color="error"
+                  size="small"
+                  @click="removeSlot(char.name)"
+                ></v-btn
+              ></template>
             </v-list-item>
           </v-list>
         </v-card-text>
-        <v-card-actions><v-spacer></v-spacer><v-btn variant="text"
-            @click="charSettingDialog = false">닫기</v-btn></v-card-actions>
+        <v-card-actions
+          ><v-spacer></v-spacer
+          ><v-btn variant="text" @click="charSettingDialog = false"
+            >닫기</v-btn
+          ></v-card-actions
+        >
       </v-card>
     </v-dialog>
   </v-app>
@@ -170,7 +301,7 @@ const showFab = ref(false);
 
 const ALLOWED_GUILD_IDS = [
   "1376571141556277380", // 기존 길드 서버
-  "1295559813001908301"  // 새로 추가할 길드 서버
+  "1295559813001908301", // 새로 추가할 길드 서버
 ];
 
 // App.vue 내 handleLogin 함수 부분
@@ -185,16 +316,16 @@ const handleLogin = async () => {
 
     // 디스코드 가입 서버 목록 요청
     const res = await axios.get("https://discord.com/api/users/@me/guilds", {
-      headers: { Authorization: `Bearer ${accessToken}` }
+      headers: { Authorization: `Bearer ${accessToken}` },
     });
-
-
 
     const guilds = res.data;
 
     console.log(guilds);
 
-    const isMember = guilds.some(guild => ALLOWED_GUILD_IDS.includes(guild.id));
+    const isMember = guilds.some((guild) =>
+      ALLOWED_GUILD_IDS.includes(guild.id),
+    );
 
     if (isMember) {
       isLoggedIn.value = true;
@@ -208,7 +339,9 @@ const handleLogin = async () => {
     // 에러 발생 시(401 포함) 무조건 로그아웃 처리하여 진입 차단
     await logout();
     isLoggedIn.value = false;
-    alert("디스코드 인증 서버와 통신 중 오류가 발생했습니다. 다시 로그인해주세요.");
+    alert(
+      "디스코드 인증 서버와 통신 중 오류가 발생했습니다. 다시 로그인해주세요.",
+    );
   }
 };
 
@@ -247,7 +380,11 @@ onMounted(() => {
 
 const setupFirestoreSnapshots = () => {
   // 빌런 추적
-  const qVillain = query(collection(db, "villains"), orderBy("reportCount", "desc"), limit(10));
+  const qVillain = query(
+    collection(db, "villains"),
+    orderBy("reportCount", "desc"),
+    limit(10),
+  );
   onSnapshot(qVillain, (snapshot) => {
     if (!snapshot.empty) {
       const maxCount = snapshot.docs[0].data().reportCount;
@@ -258,15 +395,25 @@ const setupFirestoreSnapshots = () => {
   });
 
   // 원정대 통계
-  const qTop = query(collection(db, "roster_stats"), orderBy("totalCount", "desc"), limit(1));
+  const qTop = query(
+    collection(db, "roster_stats"),
+    orderBy("totalCount", "desc"),
+    limit(1),
+  );
   onSnapshot(qTop, (topSnapshot) => {
     if (!topSnapshot.empty) {
       const maxCount = topSnapshot.docs[0].data().totalCount || 0;
       topRosterCount.value = maxCount;
-      const qAllTop = query(collection(db, "roster_stats"), where("totalCount", "==", maxCount));
+      const qAllTop = query(
+        collection(db, "roster_stats"),
+        where("totalCount", "==", maxCount),
+      );
       onSnapshot(qAllTop, (allSnapshot) => {
         let allMembers = [];
-        allSnapshot.forEach((doc) => { if (doc.data().members) allMembers = [...allMembers, ...doc.data().members]; });
+        allSnapshot.forEach((doc) => {
+          if (doc.data().members)
+            allMembers = [...allMembers, ...doc.data().members];
+        });
         topRosterMembers.value = allMembers;
       });
     }
@@ -278,7 +425,9 @@ provide("topVillains", topVillains);
 provide("topRosterMembers", topRosterMembers);
 provide("topRosterCount", topRosterCount);
 
-const getCurrentSlot = computed(() => mainCharSlots.value.find((c) => c.name === currentMainName.value));
+const getCurrentSlot = computed(() =>
+  mainCharSlots.value.find((c) => c.name === currentMainName.value),
+);
 
 const toggleTheme = () => {
   theme.value = theme.value === "light" ? "dark" : "light";
@@ -311,13 +460,17 @@ const checkWeeklyReset = () => {
         const chars = JSON.parse(savedChars);
         const resetChars = chars.map((c) => ({
           ...c,
-          completedTasks: c.completedTasks.filter((id) => ["chaos", "guardian"].includes(id)),
+          completedTasks: c.completedTasks.filter((id) =>
+            ["chaos", "guardian"].includes(id),
+          ),
         }));
         localStorage.setItem(storageKey, JSON.stringify(resetChars));
       }
     });
     localStorage.setItem("last_hw_reset", resetTime.toString());
-    window.dispatchEvent(new CustomEvent("main-char-changed", { detail: currentMainName.value }));
+    window.dispatchEvent(
+      new CustomEvent("main-char-changed", { detail: currentMainName.value }),
+    );
   }
 };
 
@@ -326,29 +479,48 @@ const addNewSlot = async () => {
   isFetching.value = true;
   try {
     const url = `/api/armories/characters/${encodeURIComponent(searchCharName.value)}/profiles`;
-    const res = await axios.get(url, { headers: { Authorization: `bearer ${API_KEY.trim()}` } });
+    const res = await axios.get(url, {
+      headers: { Authorization: `bearer ${API_KEY.trim()}` },
+    });
     if (res.data) {
       const data = res.data;
-      const newChar = { name: data.CharacterName, level: data.ItemAvgLevel, className: data.CharacterClassName, img: data.CharacterImage };
+      const newChar = {
+        name: data.CharacterName,
+        level: data.ItemAvgLevel,
+        className: data.CharacterClassName,
+        img: data.CharacterImage,
+      };
       if (!mainCharSlots.value.some((c) => c.name === newChar.name)) {
         mainCharSlots.value.push(newChar);
-        localStorage.setItem("main_char_slots", JSON.stringify(mainCharSlots.value));
+        localStorage.setItem(
+          "main_char_slots",
+          JSON.stringify(mainCharSlots.value),
+        );
         switchAccount(newChar.name);
       }
       searchCharName.value = "";
     }
-  } catch (e) { alert("캐릭터를 찾을 수 없습니다."); } finally { isFetching.value = false; }
+  } catch (e) {
+    alert("캐릭터를 찾을 수 없습니다.");
+  } finally {
+    isFetching.value = false;
+  }
 };
 
 const removeSlot = (name) => {
   if (!confirm(`${name} 슬롯을 제거하시겠습니까?`)) return;
   mainCharSlots.value = mainCharSlots.value.filter((c) => c.name !== name);
   localStorage.setItem("main_char_slots", JSON.stringify(mainCharSlots.value));
-  if (currentMainName.value === name) switchAccount(mainCharSlots.value[0]?.name || "");
+  if (currentMainName.value === name)
+    switchAccount(mainCharSlots.value[0]?.name || "");
 };
 
-const handleScroll = () => { showFab.value = window.scrollY > 300; };
-const scrollToTop = () => { window.scrollTo({ top: 0, behavior: "smooth" }); };
+const handleScroll = () => {
+  showFab.value = window.scrollY > 300;
+};
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
 
 onUnmounted(() => {
   window.removeEventListener("scroll", handleScroll);
@@ -391,5 +563,11 @@ body {
   bottom: 32px;
   right: 32px;
   z-index: 99;
+}
+
+.stamp-no-bg :deep(img) {
+  /* 이미지 자체의 미세한 검은 테두리가 있다면 1~2px 정도 안쪽으로 잘라냅니다 */
+  clip-path: inset(1px);
+  object-fit: contain;
 }
 </style>
